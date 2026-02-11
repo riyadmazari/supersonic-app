9c1f0d6c1855c71aacb80c1d503d54bb71559d67
Philine de Graaf <philinedegraaf@MacBook-Pro-von-Philine.local>
2026-02-11 10:50:16 +0100

    Session db added

---
+++ app/db/session.py
@@ -0,0 +1,7 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+
+from app.core.config import settings
+
+engine = create_async_engine(settings.DATABASE_URL, echo=False)
+
+AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
